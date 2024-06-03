a05f0b73b7a14f3477c6f63b12e2dd1ea8bd4a21
Anubhav Pal <anubhavpal97@gmail.com>
2024-06-03 22:24:57 +0530

    Updated code which can utilise tailwind to style code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 };
 
 export default App;
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="" />
+      </div>
+
+      <div className="bottom"></div>
+    </div>
+  );
 };
 
 export default Sidebar;
