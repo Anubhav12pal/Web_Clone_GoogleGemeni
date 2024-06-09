5bad13101383bf3c9f8d72f749ce67f4567f911a
Anubhav Pal <anubhavpal97@gmail.com>
2024-06-09 11:54:31 +0530

    UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Sidebar from "./Components/Sidebar/Sidebar";
-
+import Main from "./Components/Main/Main";
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
       {/* <h2 className="">hello mote</h2> */}
     </>
   );
